7de2eb147d79ee3587acb4a254c6c6b53e86c33e
kevinbacino <kevin.bacino@gmail.com>
2024-11-14 06:23:01 -0500

    remove by index method; fixed a few bugs

--- random/singlelinkedlist.py
+++ random/singlelinkedlist.py
@@ -19,7 +19,6 @@ class LinkedList:
             self.head = n
             self.tail = self.head
         else:
-            self.tail = self.head
             n.next = self.head
             self.head = n
 
@@ -44,9 +43,35 @@ class LinkedList:
             temp = temp.next
         return rtn_arr
 
+    def remove_by_index(self, index: int) -> Union[T, None]:
+        if index > -1 and self.head is not None:
+
+            temp_item = None
+            if index == 0:
+                temp_item = self.head.item
+                self.head = self.head.next
+                if self.head is None or self.head.next is None:
+                    self.tail = self.head
+
+            else:
+                cntr = 1
+                temp = self.head
+                while temp.next is not None:
+                    if cntr == index:
+                        temp_item = temp.next.item
+                        temp.next = temp.next.next
+                        if temp.next is None:
+                            self.tail = temp
+                        break
+                    temp = temp.next
+                    cntr += 1
+            return temp_item
+
     def find_first_and_remove(self, item: T) -> None:
         if self.head.item == item:
             self.head = self.head.next
+            if self.head is None or self.head.next is None:
+                self.tail = self.head
         else:
             temp = self.head
             while temp.next is not None:
@@ -58,25 +83,25 @@ class LinkedList:
                     break
                 temp = temp.next
 
-
-
-    def print(self):
+    def print(self) -> None:
         temp = self.head
+        str = ''
         while temp is not None:
-            print(temp.item)
+            str += temp.item + ' '
             temp = temp.next
+        print(str)
 
 
 l = LinkedList()
-l.add_head('h')
-l.add_head('k')
-l.add_last('r')
-l.add_last('p')
-l.add_last('w')
-l.find_first_and_remove('w')
-l.add_last('z')
-l.find_first_and_remove('z')
-l.add_last('k')
-l.find_first_and_remove('a')
-
+l.add_head('a')
+l.remove_by_index(0)
+l.add_head('a')
+l.add_last('b')
+l.remove_by_index(0)
+l.find_first_and_remove('b')
+l.add_last('a')
+l.add_last('b')
+l.add_head('c')
+l.remove_by_index(2)
+l.add_head('b')
 l.print()
